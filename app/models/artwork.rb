5bd632ce2a25ebfcca790ea54471c1166099f691
Robert Huberdeau <rhuberdeau@gmail.com>
2011-05-31 23:34:29 -0400

    Still working on ImageMagick

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,8 @@
 class Artwork < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :thumb => "215x120" }, 
+  has_attached_file :photo, 
+  					:styles => { :thumb => "215x120" }, 
   					:storage => :s3,
      				:s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
      				:path => "/:style/:id/:filename"
+     				:path => ":attachment/:id/:style/:filename"
 end
