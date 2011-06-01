68ffb186dd2eaa3b684482790cbdf95d972a79e7
Robert Huberdeau <rhuberdeau@gmail.com>
2011-05-31 22:55:14 -0400

    More FE work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+
+	$("ul#topnav li").hover(function() { //Hover over event on list item
+		$(this).css({ 'background' : '#1376c9 url(topnav_active.gif) repeat-x'}); //Add background color and image on hovered list item
+		$(this).find("span").show(); //Show the subnav
+	} , function() { //on hover out...
+		$(this).css({ 'background' : 'none'}); //Ditch the background
+		$(this).find("span").hide(); //Hide the subnav
+	});
+
+});
+
+$(document).ready(function(){
+
+    $("a.switch_thumb").toggle(function(){
+        $(this).addClass("swap");
+        $("ul.display").fadeOut("fast", function() {
+            $(this).fadeIn("fast").addClass("thumb_view");
+        });
+    }, function () {
+        $(this).removeClass("swap");
+        $("ul.display").fadeOut("fast", function() {
+            $(this).fadeIn("fast").removeClass("thumb_view");
+        });
+    }); 
+
+});
