cc1e84e3e43d5c14802861bb47d8c1d9b3a218d4
Robert Huberdeau <rhuberdeau@gmail.com>
2011-09-13 18:14:06 -0400

    Adding Production command path for paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Dan::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
@@ -22,5 +22,6 @@ Dan::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  Paperclip.options[:command_path] = "c/Program Files/ImageMagick-6.5.6-Q8"
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,5 @@ Dan::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  Paperclip.options[:command_path] = "/usr/bin/"
 end
