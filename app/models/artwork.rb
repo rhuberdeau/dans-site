e9a86da909158467d43e82cf90a97cd33b35c72f
Robert Huberdeau <rhuberdeau@gmail.com>
2011-09-13 18:39:04 -0400

    Changed resize code

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,6 @@
 class Artwork < ActiveRecord::Base
   has_attached_file :photo, 
-  					:styles => { :small => "215x120>" }, 
+  					:styles => { :small => "215x120#" }, 
   					:storage => :s3,
      				:s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
      				:path => ":attachment/:id/:style/:filename"
