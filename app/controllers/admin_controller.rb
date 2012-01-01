e6cb5e3a7366d161aa285d89df73a41867763cb9
Robert Huberdeau <rhuberdeau@gmail.com>
2012-01-01 13:03:23 -0500

    Addding to admin and readme

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   before_filter :authenticate_user!
 	
   def index
-  	
+  	@galleries = Gallery.all_cached
+  	@artworks = Artwork.all_cached
   end
 end
