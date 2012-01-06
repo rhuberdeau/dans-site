57a5360c05c05fe4c9d1bad5de82ccd4e7e0a7b9
Robert Huberdeau <rhuberdeau@gmail.com>
2012-01-05 20:24:40 -0500

    Tinkering with cache sweeper

--- app/sweepers/artwork_sweeper.rb
+++ app/sweepers/artwork_sweeper.rb
@@ -10,7 +10,7 @@ class ArtworkSweeper < ActionController::Caching::Sweeper
   end
   
   def expire_cache_for(artwork)
-  	expire_page(:controller => 'artworks', :action => 'index')
+  	expire_page :action => :index
   	expire_page(:controller => 'artworks', :action => 'show', :id => artwork)
     expire_fragment('header')
   end
